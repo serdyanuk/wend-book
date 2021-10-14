524098c569d5ae435b2fe2102d8ed98b55412ed6
serdyanuk <plutoject@gmail.com>
2021-10-14 14:41:22 +0300

    added script for deploy to github pages

---
+++ deploy-ghpages.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+set -e
+
+yarn run build
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# если вы публикуете по адресу https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:serdyanuk/wend-book.git master:gh-pages
+
+cd -
