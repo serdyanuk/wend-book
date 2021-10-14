709e4dc7707a8dfad1198482c9c09aa9bcaf6dc8
serdyanuk <plutoject@gmail.com>
2021-10-14 14:49:28 +0300

    removed comments

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -9,10 +9,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# если вы публикуете по адресу https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:serdyanuk/wend-book.git master:gh-pages
 
 cd -
