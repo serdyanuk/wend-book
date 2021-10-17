b0a140be4570d871a8f0eeded727f83a01957218
serdyanuk <plutoject@gmail.com>
2021-10-17 22:53:28 +0300

    Howler volume setted to 0.3 from 1.0

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "@/router";
 import store from "@/store";
+import { Howler } from "howler";
+
+Howler.volume(0.3);
 
 const app = createApp(App);
 app.use(router);
